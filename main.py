ffb7faa4fd266c3a85327115ff160c03fc28eb90
Arseniy-Zhuck <arseniyzhuck@mail.ru>
2024-02-01 08:57:45 +0300

    finish commit - to client

--- main.py
+++ main.py
@@ -1,26 +1,7 @@
 # This is a sample Python script.
-import json
-
-from gtts import gTTS
-from pydub import AudioSegment
-
-from exp_gTTs import make_mp3, make_mp3s_from_json, make_mp3_from_text
 
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from source.make_mp3_from_json import make_mp3s_from_json
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     make_mp3s_from_json('example_json.json')
-    # make_mp3_from_text('start', 'en', False, 'result.mp3')
-    # audio = AudioSegment.from_mp3('C:\\Users\\a_zhuck\Documents\GitHub\sound_JSON\\result.mp3')
-    # audio.export("res_name.mp3", format="mp3")
-    # collect_single_mp3_from_json('example_json.json')
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ source/make_mp3_from_json.py
@@ -0,0 +1,45 @@
+import json
+from gtts import gTTS
+
+
+# we make audio from text in var text and add binary audio to output stream f
+def make_mp3_from_text(text, lang, slow, f):
+    audio = gTTS(text=text, lang=lang, slow=slow)
+    audio.write_to_fp(f)
+
+# we make audio from data and use make_mp3_from_text
+# data can be - dictionary, list, string and integer
+# for dictionary and list we make recursive
+# for atomic types we make audio in ru lang
+def make_mp3(data, f):
+
+    if type(data) == dict:
+        dictionary = data
+        make_mp3_from_text(text="словарь", lang="ru", slow=False, f=f)
+        for key in dictionary:
+            str_to_mp3 = str(key)
+            make_mp3_from_text(text=str_to_mp3, lang="en", slow=False, f=f)
+            make_mp3(dictionary[key], f)
+            make_mp3_from_text(text=str_to_mp3 + " значение закончено", lang="en", slow=False, f=f)
+    elif type(data) == list:
+        list_values = data
+        make_mp3_from_text(text="список", lang="ru", slow=False, f=f)
+        n = 0
+        for element in list_values:
+            make_mp3(element, f)
+            n += 1
+    else:
+        str_to_mp3 = str(data)
+        make_mp3_from_text(text=str_to_mp3, lang="ru", slow=False, f=f)
+
+# read json in dictionary
+# open audio binary stream for output
+# use make_mp3
+def make_mp3s_from_json(json_path):
+    with open(json_path, encoding='utf-8') as example_file:
+        data = json.load(example_file)
+    with open('source/result.mp3', 'wb') as f:
+        make_mp3(data, f)
+
+
+
